3b0b372c9cf6056cdce0206cdd2f5cdf3837cf01
David Trautmann <david.trautmann@wika.com>
2015-03-05 15:00:17 +0100

    Created nunit test project.
    added first nunit test and create yml file for travis ci.

---
+++ Wox.Plugin.Lync.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("Wox.Plugin.Lync.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WIKA")]
+[assembly: AssemblyProduct("Wox.Plugin.Lync.Tests")]
+[assembly: AssemblyCopyright("Copyright © WIKA 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("0a4e7e78-43e1-46a6-a644-5c0292117dfb")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Wox.Plugin.Lync.Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Wox.Plugin;
+
+namespace Wox.Plugin.Lync.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void emptyQuery()
+        {
+            Lync lyncPlugin = new Lync();
+            Query query = new Query(String.Empty);
+            List<Result> results = lyncPlugin.Query(query);
+
+            Assert.AreEqual(results.Count, 0);
+        }
+    }
+}
